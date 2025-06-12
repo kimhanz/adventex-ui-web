c4957afa51d8cc12c0e7c5e83443b4e56aa27f11
khaoxkongz <makkawal.thanapat@gmail.com>
2025-06-12 21:40:29 +0700

    feat(storage): implement cloud storage system - Add S3 and UploadThing providers

--- payload.config.ts
+++ payload.config.ts
@@ -1,12 +1,15 @@
-// storage-adapter-import-placeholder
 import path from "path"
 import { fileURLToPath } from "url"
 import { mongooseAdapter } from "@payloadcms/db-mongodb"
-import { payloadCloudPlugin } from "@payloadcms/payload-cloud"
+import { resendAdapter } from "@payloadcms/email-resend"
 import { lexicalEditor } from "@payloadcms/richtext-lexical"
+import { s3Storage } from "@payloadcms/storage-s3"
+import { uploadthingStorage } from "@payloadcms/storage-uploadthing"
 import { buildConfig } from "payload"
 import sharp from "sharp"
 
+import { BlogPosts } from "./collections/BlogPosts"
+import { Documents } from "./collections/Documents"
 import { Galleries } from "./collections/Galleries"
 import { Media } from "./collections/Media"
 import { TourStudies } from "./collections/TourStudies"
@@ -23,18 +26,56 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, TourStudies, TourTravels, Galleries],
+  cors: ["http://localhost:3000", process.env.NEXT_PUBLIC_APP_URL!],
+  csrf: ["http://localhost:3000", process.env.NEXT_PUBLIC_APP_URL!],
+  collections: [
+    Users,
+    Media,
+    Documents,
+    TourStudies,
+    TourTravels,
+    Galleries,
+    BlogPosts,
+  ],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || "",
+  secret: process.env.PAYLOAD_SECRET!,
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI || "",
+    url: process.env.DATABASE_URI!,
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      bucket: process.env.S3_BUCKET!,
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET!,
+        },
+        endpoint: process.env.S3_ENDPOINT!,
+        region: "auto",
+      },
+    }),
+    uploadthingStorage({
+      enabled: true,
+      collections: {
+        documents: true,
+      },
+      options: {
+        token: process.env.UPLOADTHING_TOKEN!,
+        acl: "public-read",
+      },
+    }),
   ],
+  email: resendAdapter({
+    defaultFromName: "Adventex",
+    defaultFromAddress: "support@adventex.co.th",
+    apiKey: process.env.RESEND_API_KEY!,
+  }),
 })
